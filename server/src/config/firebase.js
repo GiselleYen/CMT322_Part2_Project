77b7dfe845da68fd4b27a2637f3d8d9677ddc3c3
GiselleYen <giselleyen02@gmail.com>
2024-12-23 15:08:29 +0800

    add client id

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -7,6 +7,7 @@ const serviceAccount = {
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
 };
 
 // Handle private key formatting
